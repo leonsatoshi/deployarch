054e4356c4d95b4866189e623583c5d6177296c3
Leon <leonsatoshi39@gmail.com>
2024-10-09 20:03:19 +0900

    split to gentoo-x13 branch

---
+++ gentoo/ch_gentoo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cp --dereference /etc/resolv.conf /gentoo/etc/
+mount -t proc /proc /gentoo/proc
+mount --rbind /sys  /gentoo/sys
+mount --make-rslave /gentoo/sys
+mount --rbind /dev  /gentoo/dev
+mount --make-rslave /gentoo/dev
+mount --rbind /run  /gentoo/run
+mount --make-rslave /gentoo/run
+
+chroot /gentoo /bin/bash
